09334e49d6041a2ff9213357ce1a448909075cf7
Augusto Gomez <agomezsantos95@gmail.com>
2023-11-12 23:43:45 +0100

    Fix testing suite for AddUserPage

--- src/pages/AddUserPage/AddUserPage.test.tsx
+++ src/pages/AddUserPage/AddUserPage.test.tsx
@@ -1,14 +1,15 @@
-import { render, screen } from "@testing-library/react";
+import { screen } from "@testing-library/react";
 import AddUserPage from "./AddUserPage";
 import { ThemeProvider } from "styled-components";
 import mainTheme from "../../styles/mainTheme";
+import customRender from "../../mocks/utils/testUtils";
 
 describe("Given an AddUserPage component", () => {
   describe("When it is rendered", () => {
     test("Then it should show a 'Add your own user to the page' title on a heading", () => {
       const titleText = "Add your own user to the page";
 
-      render(
+      customRender(
         <ThemeProvider theme={mainTheme}>
           <AddUserPage />
         </ThemeProvider>,
