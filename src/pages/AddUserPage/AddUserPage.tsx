735bd59eaffbe876c7ae547d5564869d2ec062f8
Augusto Gomez <agomezsantos95@gmail.com>
2023-11-12 17:11:42 +0100

    Add AdduserPage component to the page

---
+++ src/pages/AddUserPage/AddUserPage.tsx
@@ -0,0 +1,9 @@
+import AddUserPageStyled from "./AddUserPageStyled";
+
+const AddUserPage = () => (
+  <AddUserPageStyled className="form-container">
+    <h2 className="page-title">Add your own user to the page</h2>
+  </AddUserPageStyled>
+);
+
+export default AddUserPage;
