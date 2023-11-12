f832114c24f5917ed9679ed9dfcf2bcaee02d931
Augusto Gomez <agomezsantos95@gmail.com>
2023-11-12 13:18:16 +0100

    Add testing suite for setFriendStatus

--- src/components/UserCard/UserCard.test.tsx
+++ src/components/UserCard/UserCard.test.tsx
@@ -1,19 +1,14 @@
-import { render, screen } from "@testing-library/react";
+import { screen } from "@testing-library/react";
 import UserCard from "./UserCard";
 import userMock from "../../mocks/userMocks/userMock";
-import { ThemeProvider } from "styled-components";
-import mainTheme from "../../styles/mainTheme";
+import customRender from "../../mocks/utils/testUtils";
 
 describe("Given a UserCard component", () => {
   describe("When it receives a user Jeff Winger", () => {
     test("Then it will show 'Jeff Winger' on a heading", () => {
       const userNameTitle = "Jeff Winger";
 
-      render(
-        <ThemeProvider theme={mainTheme}>
-          <UserCard user={userMock} />
-        </ThemeProvider>,
-      );
+      customRender(<UserCard user={userMock} />);
 
       const title = screen.getByRole("heading", { name: userNameTitle });
 
@@ -23,11 +18,7 @@ describe("Given a UserCard component", () => {
     test("Then it will show the alternative text 'Close up of Jeff Winger' on an image", () => {
       const userImageText = "Close up of Jeff Winger";
 
-      render(
-        <ThemeProvider theme={mainTheme}>
-          <UserCard user={userMock} />
-        </ThemeProvider>,
-      );
+      customRender(<UserCard user={userMock} />);
 
       const image = screen.getByRole("img", { name: userImageText });
 
--- src/store/features/userSlice/userSlice.test.ts
+++ src/store/features/userSlice/userSlice.test.ts
@@ -1,7 +1,12 @@
 import usersMock from "../../../mocks/userMocks/userMocks";
-import { UsersStructure, loadUsers, usersReducer } from "./userSlice";
+import {
+  UsersStructure,
+  loadUsers,
+  setFriendStatus,
+  usersReducer,
+} from "./userSlice";
 
-describe("Given a usersRecucer", () => {
+describe("Given a usersReducer", () => {
   describe("When it receives users Jeff and Britta and an intention to add them to the store", () => {
     test("Then it should add Jeff and Britta to the store", () => {
       const previousState: UsersStructure = { users: [] };
@@ -12,6 +17,19 @@ describe("Given a usersRecucer", () => {
       );
 
       expect(actualUsersState.users).toStrictEqual(usersMock);
+    });
+  });
+
+  describe("When it receives user Jeff and Britta and an intention to change the friend status of Jeff", () => {
+    test("Then it should change Jeff friend status", () => {
+      const previousState: UsersStructure = { users: usersMock };
+
+      const actualUserState = usersReducer(
+        previousState,
+        setFriendStatus(usersMock[0].id),
+      );
+
+      expect(actualUserState.users[0].isFriend).toBe(false);
     });
   });
 });
